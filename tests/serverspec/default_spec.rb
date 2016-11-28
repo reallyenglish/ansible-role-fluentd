506ce95a8db4e1ac7636f91780aee58f71a2f742
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2016-11-28 15:32:56 +0900

    support ubuntu

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -1,24 +1,26 @@
 require 'spec_helper'
 require 'serverspec'
 
-fluentd_package_name = 'fluentd'
-fluentd_service_name = 'fluentd'
-fluentd_config       = '/etc/fluentd/fluentd.conf'
-fluentd_user_name    = 'fluentd'
-fluentd_user_group   = 'fluentd'
-fluentd_work_dir     = '/var/spool/fluentd'
-fluentd_gem_bin      = '/usr/bin/fluent-gem'
-fluentd_certs_dir    = '/etc/fluentd/certs'
+fluentd_package_name = 'td-agent'
+fluentd_service_name = 'td-agent'
+fluentd_config_dir   = '/etc/td-agent/conf.d'
+fluentd_config       = '/etc/td-agent/td-agent.conf'
+fluentd_user_name    = 'td-agent'
+fluentd_user_group   = 'td-agent'
+fluentd_gem_bin      = '/usr/sbin/td-agent-gem'
+fluentd_certs_dir    = '/etc/td-agent/certs'
 fluentd_buffer_dir   = '/var/spool/fluentd'
 fluentd_unix_pipe_dir= '/var/tmp/fluentd'
 
 case os[:family]
 when 'freebsd'
+  fluentd_user_name    = 'fluentd'
+  fluentd_user_group   = 'fluentd'
   fluentd_package_name = 'rubygem-fluentd'
   fluentd_service_name = 'fluentd'
   fluentd_config_path  = '/usr/local/etc/fluentd/fluent.conf'
   fluentd_config_dir   = '/usr/local/etc/fluentd/conf.d'
-  fluent_gem_bin       = '/usr/local/bin/fluent-gem'
+  fluentd_gem_bin       = '/usr/local/bin/fluent-gem'
   fluentd_certs_dir    = '/usr/local/etc/fluentd/certs'
 end
 
@@ -36,7 +38,7 @@ describe service(fluentd_service_name) do
   it { should be_enabled }
 end
 
-describe command("#{fluent_gem_bin} list") do
+describe command("#{fluentd_gem_bin} list") do
   its(:stdout) { should match /fluent-plugin-redis/ }
   its(:stdout) { should match /fluent-plugin-secure-forward/ }
 end
